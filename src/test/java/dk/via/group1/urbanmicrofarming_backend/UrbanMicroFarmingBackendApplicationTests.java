ade2fc41165ad5c8c942ea4b61ccc28dba790e7f
yousseftopaji <youssefftopaji@gmail.com>
2026-04-13 10:20:56 +0200

    initial commit

---
+++ src/test/java/dk/via/group1/urbanmicrofarming_backend/UrbanMicroFarmingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.via.group1.urbanmicrofarming_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class UrbanMicroFarmingBackendApplicationTests
+{
+
+  @Test void contextLoads()
+  {
+  }
+
+}
