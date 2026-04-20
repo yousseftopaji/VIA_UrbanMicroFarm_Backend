6ce4efd26cf26369af16bb2ee4145e683c5adf1c
Samojak <samo.jakabovic11@gmail.com>
2026-04-20 11:51:40 +0200

    Minimal Domain for the UseCase

---
+++ src/main/java/logic/domain/GrowingSetup.java
@@ -0,0 +1,25 @@
+package logic.domain;
+
+public class GrowingSetup {
+    private final int setupId;
+    private final String serialNumber;
+    private final String location;
+
+    public GrowingSetup(int setupId, String serialNumber, String location) {
+        this.setupId = setupId;
+        this.serialNumber = serialNumber;
+        this.location = location;
+    }
+
+    public int getSetupId() {
+        return setupId;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
---
+++ src/main/java/logic/domain/SensorType.java
@@ -0,0 +1,8 @@
+package logic.domain;
+
+public enum SensorType {
+    TEMPERATURE,
+    HUMIDITY,
+    LIGHT,
+    SOIL_MOISTURE
+}
