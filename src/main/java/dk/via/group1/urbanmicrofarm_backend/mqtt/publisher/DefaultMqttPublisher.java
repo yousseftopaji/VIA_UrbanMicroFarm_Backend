e9372b8284789a5c8df557f95b943bf3f7cb4155
yousseftopaji <youssefftopaji@gmail.com>
2026-04-26 18:38:47 +0200

    feat(mqtt): Enhance DefaultMqttClientFactory to support separate publisher and subscriber clients

--- src/main/java/dk/via/group1/urbanmicrofarm_backend/mqtt/publisher/DefaultMqttPublisher.java
+++ src/main/java/dk/via/group1/urbanmicrofarm_backend/mqtt/publisher/DefaultMqttPublisher.java
@@ -2,7 +2,6 @@ package dk.via.group1.urbanmicrofarm_backend.mqtt.publisher;
 
 import dk.via.group1.urbanmicrofarm_backend.mqtt.client.MqttClientFactory;
 import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.stereotype.Component;
@@ -20,19 +19,10 @@ public class DefaultMqttPublisher implements MqttPublisher {
 
   @Override
   public void publish(String topic, String payload) throws MqttException {
-    MqttClient client = clientFactory.createClient();
-
-    MqttConnectOptions options = new MqttConnectOptions();
-    options.setAutomaticReconnect(false);
-    options.setCleanSession(true);
-    options.setConnectionTimeout(10);
-
+    MqttClient client = clientFactory.createPublisherClient();
     try {
-      client.connect(options);
-
       MqttMessage message = new MqttMessage(payload.getBytes(StandardCharsets.UTF_8));
       message.setQos(1);
-
       client.publish(topic, message);
     } finally {
       if (client.isConnected()) {
