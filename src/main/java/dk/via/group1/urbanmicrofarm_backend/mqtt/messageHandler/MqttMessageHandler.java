1bf765b957e099aaf16af0ed05165ba516a6c9d3
yousseftopaji <youssefftopaji@gmail.com>
2026-04-26 18:41:04 +0200

    feat(mqtt): Remove unnecessary @Service annotation from MqttMessageHandler interface

--- src/main/java/dk/via/group1/urbanmicrofarm_backend/mqtt/messageHandler/MqttMessageHandler.java
+++ src/main/java/dk/via/group1/urbanmicrofarm_backend/mqtt/messageHandler/MqttMessageHandler.java
@@ -2,7 +2,6 @@ package dk.via.group1.urbanmicrofarm_backend.mqtt.messageHandler;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public interface MqttMessageHandler
 {
   void handle(String topic, String payload);
