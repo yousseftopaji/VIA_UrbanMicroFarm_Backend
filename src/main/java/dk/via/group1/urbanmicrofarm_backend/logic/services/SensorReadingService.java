57f3c819445db8ca0f5cd67e374ea89137683c9e
Samojak <samo.jakabovic11@gmail.com>
2026-04-29 10:21:38 +0200

    Remove duplicate logic sensor reading service

--- src/main/java/dk/via/group1/urbanmicrofarm_backend/logic/services/SensorReadingService.java
+++
@@ -1,7 +0,0 @@
-package dk.via.group1.urbanmicrofarm_backend.logic.services;
-
-import dk.via.group1.urbanmicrofarm_backend.dto.TelemetryData;
-
-public interface SensorReadingService {
-    void processReadings(TelemetryData telemetryData);
-}
