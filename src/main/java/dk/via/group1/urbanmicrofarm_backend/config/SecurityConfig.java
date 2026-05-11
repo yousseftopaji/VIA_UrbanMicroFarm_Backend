ce74da2ffb11019a505ba813b4d7c6eb8814fbca
Aga Furgalska <aga.furgalska@wp.pl>
2026-05-11 23:36:44 +0200

    feat(SecurityConfig): added SecurityConfig to allow testing endpoints.
    Took 57 seconds

---
+++ src/main/java/dk/via/group1/urbanmicrofarm_backend/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package dk.via.group1.urbanmicrofarm_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+//    TODO: change accordingly when working on AUTH
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+}
