a496f099faba37c5b6b651534759a67747f7039d
Samojak <samo.jakabovic11@gmail.com>
2026-05-13 12:03:28 +0200

    some time thingy fix

--- src/main/java/dk/via/group1/urbanmicrofarm_backend/application/services/sensor_reading_service/SensorReadingServiceImpl.java
+++ src/main/java/dk/via/group1/urbanmicrofarm_backend/application/services/sensor_reading_service/SensorReadingServiceImpl.java
@@ -10,6 +10,7 @@ import dk.via.group1.urbanmicrofarm_backend.dto.TelemetryData;
 import dk.via.group1.urbanmicrofarm_backend.mapper.dbMapper.SensorReadingPersistenceMapper;
 import org.springframework.stereotype.Service;
 
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
